3a626150923b61cf667c6c5da4c60af9bc77c1dc
admin-sys <h.saitou@thekiss.co.jp>
2011-06-23 14:39:07 +0900

    部門名クリーニングを修正

--- app/controllers/components/cleaning.php
+++ app/controllers/components/cleaning.php
@@ -19,9 +19,29 @@ class CleaningComponent extends Object {
 		//$section_name = str_replace('KOREA', 'KR', $section_name);
 		$section_name = str_replace('イオンモール', 'イオン', $section_name);
 		$section_name = str_replace('ららぽーと', '', $section_name);
+		//ここから無理やり系
 		$section_name = str_replace('MACAU girl’s talk！ MACAO', 'マカオGT', $section_name);
-		$section_name = str_replace('MACAU THE KISS MACAO', 'マカオKISS', $section_name);
-		$section_name = str_replace('KOREA THE KISS COEX-MALL', '韓国COEX', $section_name);
+		$section_name = str_replace('MACAU  MACAO', 'マカオKISS', $section_name);
+		$section_name = str_replace('KOREA  COEX-MALL', 'COEX', $section_name);
+		$section_name = str_replace('kp　お台場メディアージュ', 'カピオお台場', $section_name);
+		$section_name = str_replace('select エミフルＭＡＳＡＫＩ', 'エミフル', $section_name);
+		$section_name = str_replace('an横浜店', 'らら横浜', $section_name);
+		$section_name = str_replace('アミュプラザ鹿児島', 'アミュ鹿児島', $section_name);
+		$section_name = str_replace('東京ドームラクーア', 'ラクーア', $section_name);
+		$section_name = str_replace('ＴＯＫＹＯ－ＢＡＹ', 'ららぽーと', $section_name);
+		$section_name = str_replace('千葉オーロラモールジュンヌ店', '千葉オーロラ', $section_name);
+		$section_name = str_replace('an お台場メディアージュ', 'anお台場', $section_name);
+		$section_name = str_replace('お台場メディアージュ', 'お台場カ', $section_name);
+		$section_name = str_replace('北九州リバーウォーク', '北九州リバー', $section_name);
+		$section_name = str_replace('outlet マリノアシティ福岡', 'outletマリノア', $section_name);
+		$section_name = str_replace('outlet 那須ガーデン', 'outlet那須', $section_name);
+		$section_name = str_replace('select 越谷レイクタウン', 'select越谷', $section_name);
+		$section_name = str_replace('an 金沢フォーラス', 'an金沢', $section_name);
+		$section_name = str_replace('ヴィーナスフォート', 'ヴィーナス', $section_name);
+		$section_name = str_replace('福岡キャナルシティ', '福岡キャナル', $section_name);
+		$section_name = str_replace('天神インキューブ', '天神インキュ', $section_name);
+		$section_name = str_replace('梅田コムサストア', '梅田コムサ', $section_name);
+		$section_name = str_replace('横浜赤レンガ倉庫', '横浜赤レンガ', $section_name);
 		
         return $section_name;
     }
